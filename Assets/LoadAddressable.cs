ca54e19231ac10fa0cb03998a57b4cb1261f0471
iridon0920 <iridon0920@gmail.com>
2023-02-03 22:26:00 +0900

    update: asset

--- Assets/LoadAddressable.cs
+++ Assets/LoadAddressable.cs
@@ -7,6 +7,16 @@ public class LoadAddressable : MonoBehaviour
 { 
     async void Start()
     {
+        // Addresables 初期化
+        await Addressables.InitializeAsync().Task;
+
+        // リモートカタログに変更があれば更新する
+        var catalogs = await Addressables.CheckForCatalogUpdates(true).Task;
+        if (catalogs != null && catalogs.Count > 0)
+        {
+            var locators = await Addressables.UpdateCatalogs(catalogs, true).Task;
+        }
+        
         var prefab = await Addressables.LoadAssetAsync<GameObject>("Assets/Methoko/methoko_1st.prefab").Task;
         Instantiate(prefab, new Vector3(0.0f, 0.0f , -8.0f), Quaternion.Euler(0f, 180f, 0f));
     }
