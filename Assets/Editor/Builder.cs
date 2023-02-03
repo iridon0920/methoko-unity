acaaf90ab61fcfb24783e13eaa32846d7aac4976
iridon0920 <iridon0920@gmail.com>
2023-02-03 11:27:59 +0900

    update: ビルド設定等

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -5,17 +5,5 @@ public class Builder
     public static void Build()
     {
         UnityEditor.AddressableAssets.Settings.AddressableAssetSettings.BuildPlayerContent();
-
-        string[] scenes =
-        {
-            "Assets/Scenes/SampleScene.unity"
-        };
-        var options = new BuildPlayerOptions
-        {
-            target = BuildTarget.WebGL,
-            locationPathName = "./Build",
-            scenes = scenes
-        };
-        BuildPipeline.BuildPlayer(options);
     }
 }
