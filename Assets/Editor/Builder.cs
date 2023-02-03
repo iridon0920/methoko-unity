5afe47fc2c7280aea69f48edb96bd21e62af23a0
iridon0920 <iridon0920@gmail.com>
2023-02-03 18:20:11 +0900

    update: Addressables等

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -1,9 +1,22 @@
+using System;
+using System.IO;
 using UnityEditor;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Build;
+using UnityEngine;
 
 public class Builder
 {
     public static void Build()
     {
-        UnityEditor.AddressableAssets.Settings.AddressableAssetSettings.BuildPlayerContent();
+        var contentStatePath = "Assets/AddressableAssetsData/Windows/addressables_content_state.bin";
+        if (File.Exists(contentStatePath))
+        {
+            ContentUpdateScript.BuildContentUpdate(AddressableAssetSettingsDefaultObject.Settings, contentStatePath);   
+        }
+        else
+        {
+            UnityEditor.AddressableAssets.Settings.AddressableAssetSettings.BuildPlayerContent();
+        }
     }
 }
